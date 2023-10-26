b29e299e8eb5376256c3a35da0015533eec7e9cd
jun.morita <jun.morita@gree.net>
2023-10-26 22:15:19 +0900

    create UIMenuSampleViewController

---
+++ UIKitSample/UIMenu/UIMenuSampleViewController.swift
@@ -0,0 +1,31 @@
+//
+//  UIMenuSampleViewController.swift
+//  UIKitSample
+//
+//  Created by Jun Morita on 2023/10/26.
+//
+
+import UIKit
+
+class UIMenuSampleViewController: UIViewController {
+
+    @IBOutlet private var button: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- UIKitSample/ViewController.swift
+++ UIKitSample/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private var tableView: UITableView!
+    private let items: [String] = [
+        "UIMenu"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
+}
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.row {
+        case 0:
+            let vc: UIViewController = UIStoryboard(name: "UIMenuSampleViewController", bundle: nil).instantiateViewController(identifier: "UIMenuSampleViewController") { coder in
+                    return UIMenuSampleViewController(coder: coder)
+            }
+            present(vc, animated: true, completion: nil)
+        default:
+            break
+        }
+    }
+    
 }
 
