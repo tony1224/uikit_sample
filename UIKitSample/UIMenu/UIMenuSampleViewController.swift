53f88104c2dc8c7cb64f81f2ec2c679bec7299d5
jun.morita <jun.morita@gree.net>
2023-10-26 23:51:56 +0900

    fix UIMenuViewController

--- UIKitSample/UIMenu/UIMenuSampleViewController.swift
+++ UIKitSample/UIMenu/UIMenuSampleViewController.swift
@@ -8,24 +8,48 @@
 import UIKit
 
 class UIMenuSampleViewController: UIViewController {
-
-    @IBOutlet private var button: UIButton!
+    
+    @IBOutlet private var button: UIButton! {
+        didSet {
+            var config = UIButton.Configuration.plain()
+            config.title = "Button"
+            config.image = UIImage(resource: .arrow)
+            config.imagePadding = 8
+            config.imagePlacement = .trailing
+            config.baseForegroundColor = .black
+            button.configuration = config
+        }
+    }
+    
+    private enum MenuType: String, CaseIterable {
+        case all = "All"
+        case myPoint = "myPoint"
+        case pointOff = "PointOff"
+        case ePoint = "EPoint"
+        case d1Point = "D1Point"
+    }
+    private var selectedMenuTyep = MenuType.all
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        configureMenuButton()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func configureMenuButton() {
+        var actions = [UIMenuElement]()
+        MenuType.allCases.forEach {
+            actions.append(configureAction(type: $0))
+        }
+        button.menu = UIMenu(title: "", options: .displayInline, children: actions)
+        button.showsMenuAsPrimaryAction = true
+        button.setTitle(selectedMenuTyep.rawValue, for: .normal)
     }
-    */
+    
+    private func configureAction(type: MenuType) -> UIAction {
+        UIAction(title: type.rawValue, image: nil, state: selectedMenuTyep == type ? .on : .off, handler: { _ in
+            self.selectedMenuTyep = type
+            self.configureMenuButton()
+        })
+    }
 
 }
